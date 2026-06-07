5b3ccd3e75439f152ae2a1522da0859663a52ce6
Kristian Skov <kris701kj@gmail.com>
2026-06-07 08:57:53 +0200

    Undo autoformat

--- src/client/settings/general/tab.ts
+++ src/client/settings/general/tab.ts
@@ -7,14 +7,14 @@ import {
   POST_METHOD_ENABLED,
   THEME_KEY,
 } from "../../constants";
-import { restartWizard } from "../../modules/wizard/wizard";
-import type { ToggleOpts } from "../../types/settings-section";
 import { getBase } from "../../utils/base-url";
 import { idbGet, idbSet } from "../../utils/db";
-import { escapeHtml } from "../../utils/dom";
 import { requestInstallPrompt } from "../../utils/install-prompt";
-import { getStoredToken } from "../../utils/settings-token";
 import { applyTheme } from "../../utils/theme";
+import { restartWizard } from "../../modules/wizard/wizard";
+import { escapeHtml } from "../../utils/dom";
+import { getStoredToken } from "../../utils/settings-token";
+import type { ToggleOpts } from "../../types/settings-section";
 import { renderSection, renderToggle } from "../shared/section";
 
 const t = window.scopedT("core");
